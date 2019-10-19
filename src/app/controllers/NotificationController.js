cde0ceb0d2065086c3f6fd11a55ee3bc9dd87221
Alan Goncalves <alan.f.goncalves@hotmail.com>
2019-10-19 15:38:33 +0100

    Complete trainer 03

---
+++ src/app/controllers/NotificationController.js
@@ -0,0 +1,38 @@
+import User from '../models/User';
+import Notification from '../schemas/Notification';
+
+class NotificationController {
+  async index(req, res) {
+    const isProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Only provider can load notifications' });
+    }
+
+    const notifications = await Notification.find({
+      user: req.userId,
+    })
+      .sort({ createAt: -1 })
+      .limit(20);
+
+    return res.json(notifications);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const notification = await Notification.findByIdAndUpdate(
+      id,
+      {
+        read: true,
+      },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
+}
+
+export default new NotificationController();
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
-
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -21,12 +21,17 @@ class UserController {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
-      return res.status(400).json({ error: 'User already exists' });
+      return res.status(400).json({ error: 'User already exists.' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
 
-    return res.json({ id, name, email, provider });
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 
   async update(req, res) {
@@ -53,9 +58,7 @@ class UserController {
     const user = await User.findByPk(req.userId);
 
     if (email !== user.email) {
-      const userExists = await User.findOne({
-        where: { email },
-      });
+      const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
         return res.status(400).json({ error: 'User already exists.' });
@@ -66,9 +69,24 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
 
-    return res.json({ id, name, email, provider });
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar,
+    });
   }
 }
 
